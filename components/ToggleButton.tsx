6e8241ebb3856e39388e1cf4c1fcb95ac592f371
jainChetan81 <jain.cj.chetan@gmail.com>
2021-10-18 18:22:09 +0530

    changes for amplify deploy

--- components/ToggleButton.tsx
+++ components/ToggleButton.tsx
@@ -2,7 +2,7 @@ import PropTypes from "prop-types";
 import styles from "../styles/Toggle.module.css";
 
 const ToggleButton = ({ clicked }: any) => (
-	<div onMouseDown={clicked} className={styles.toggle}>
+	<div onMouseDown={clicked} className={styles.toggle} role="button" tabIndex={0}>
 		<input type="checkbox" className={styles.checkbox} aria-label="Toggle Dark Mode" />
 		<div className={styles.knobs} />
 		<div className={styles.layer} />
