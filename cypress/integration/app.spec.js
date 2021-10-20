8add483d0f279051eefe6a37e435a38db1663e2b
jainChetan81 <jain.cj.chetan@gmail.com>
2021-10-21 02:39:18 +0530

    removed old npx commands with underlying npm commands

--- cypress/integration/app.spec.js
+++ cypress/integration/app.spec.js
@@ -8,13 +8,15 @@ describe("check for basic tests", () => {
 	it("elements present on the Home page", () => {
 		// Start from the Home page
 		cy.contains("h1", /Pokemon Gallery/i);
-		cy.get("[data-testid='pokemon']").should("have.length", 30);
+		cy.get("[data-testid='pokemon-card']").should("have.length", 30);
 		cy.scrollTo("bottom");
-		cy.get("[data-testid='pokemon']").should("have.length", 60);
+		cy.get("[data-testid='pokemon-card']").should("have.length", 60);
 	});
 	it("hovering on any pokemon card", () => {
-		cy.get("[data-testid='pokemon']").first().trigger("mouseover");
+		cy.get("[data-testid='pokemon-card']").first().trigger("mouseover");
 		cy.get("a[aria-label='Go to bulbasaur details']").click();
+		// eslint-disable-next-line cypress/no-unnecessary-waiting
+		cy.wait(8000);
 		cy.url().should("include", "pokemon/1");
 		cy.contains("h1", /bulbasaur/i);
 		cy.get("img").should("have.attr", "alt");
